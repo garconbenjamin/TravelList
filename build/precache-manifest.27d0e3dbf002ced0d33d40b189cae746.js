03139ba4a9075dce22d9920d7a9e9b63666aa678
chinyu_official <chinyu.offical@gmail.com>
2020-01-22 05:56:22 +0800

    build

---
+++ build/precache-manifest.27d0e3dbf002ced0d33d40b189cae746.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd574b0817f9968e627c0e7410ef982f",
+    "url": "/TravelList/index.html"
+  },
+  {
+    "revision": "6285f7dfd3de35c98d19",
+    "url": "/TravelList/static/css/main.c88ddca0.chunk.css"
+  },
+  {
+    "revision": "3943d598551f17f10b2a",
+    "url": "/TravelList/static/js/2.5d3d466f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/TravelList/static/js/2.5d3d466f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6285f7dfd3de35c98d19",
+    "url": "/TravelList/static/js/main.2d5cd86e.chunk.js"
+  },
+  {
+    "revision": "91dd1b1c0418b0bb21f8",
+    "url": "/TravelList/static/js/runtime-main.95272428.js"
+  }
+]);
